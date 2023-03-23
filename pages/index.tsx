b472063d13b82a8fb4fc4e1a2fc7aac0137fd5af
Italo Teixeira <italoteixeira@SouthPoles-MacBook-Air.local>
2023-03-23 23:26:13 +0100

    style: add basic styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,36 @@
+import { HomeContainer, Product } from "@/styles/pages/home";
+import Image from "next/image";
+import { useKeenSlider } from 'keen-slider/react'
+
+import 'keen-slider/keen-slider.min.css'
+
 export default function Home() {
-  return (<h1>Hello world</h1>)
+	const [sliderRef] = useKeenSlider({
+		slides: {
+			perView: 3,
+			spacing: 48
+		}
+	});
+
+	return (
+		<HomeContainer ref={sliderRef} className="keen-slider">
+			<Product className="keen-slider__slide">
+				<Image src={camiseta1} width={520} height={480} alt="" />
+
+				<footer>
+				<strong>Camiseta X</strong>
+				<span>R$ 79,90</span>
+				</footer>
+			</Product>
+
+			<Product className="keen-slider__slide">
+				<Image src={camiseta2} width={520} height={480} alt="" />
+
+				<footer>
+				<strong>Camiseta X</strong>
+				<span>R$ 79,90</span>
+				</footer>
+			</Product>
+		</HomeContainer>
+	)
 }
