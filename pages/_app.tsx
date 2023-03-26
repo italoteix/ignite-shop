d0c77dcbdd7e2f016594646f4ae060e7147ba0ab
Italo Teixeira <italoteixeira@SouthPoles-MacBook-Air.local>
2023-03-26 15:12:51 +0200

    feat: add use-shopping-cart

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,17 +3,31 @@ import { Container, Header } from '@/styles/pages/app'
 import logoImg from "@/assets/logo.svg"
 import type { AppProps } from 'next/app'
 import Image from 'next/image'
+import { CartProvider } from 'use-shopping-cart'
 
 globalStyles()
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-		<Container>
-			<Header>
-				<Image src={logoImg} alt="" />
-			</Header>
+const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`
+const cancelUrl = `${process.env.NEXT_URL}/`
 
-			<Component {...pageProps} />
-		</Container>
+export default function App({ Component, pageProps }: AppProps) {
+	return (
+		<CartProvider
+			mode='payment'
+			cartMode='client-only'
+			stripe={process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string}
+			successUrl={successUrl}
+			cancelUrl={cancelUrl}
+			currency='BRL'
+			shouldPersist
+		>
+			<Container>
+				<Header>
+					<Image src={logoImg} alt="" />
+				</Header>
+
+				<Component {...pageProps} />
+			</Container>
+		</CartProvider>
 	)
 }
